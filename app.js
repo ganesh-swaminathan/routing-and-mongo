b70bef7f35a4544016853f24ae598f169f4420fa
Ganesh <gbalaji.4567@gmail.com>
2023-11-25 19:11:13 +0530

    removed unwanted libraries

--- app.js
+++ app.js
@@ -1,15 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const http = require('http')
-const path = require('path')
-const urlencoded = require('url')
-const json = require('json')
-const logger = require('logger')
-const moverride = require('method-override')
-const fs = require('fs');
-
 const routes = require('./routes')
-const connectToMongoDB = require("./db");
 const dbMiddleware = require('./dbMiddleware');
 
 var app = express();
